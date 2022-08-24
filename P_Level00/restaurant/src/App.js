cf7e2e205c273af97916b17e0b55480ac6b97a43
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-08-24 17:31:51 +0100

    restaurant

---
+++ P_Level00/restaurant/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home'
+
+function App() {
+  return (
+    <div className="App">
+
+      <Home/>
+
+    </div>
+  );
+}
+
+export default App;
