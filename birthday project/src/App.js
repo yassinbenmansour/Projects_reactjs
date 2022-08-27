6e706bbfcd0207c03064be11c1d402bc086ec6fe
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-08-28 00:42:15 +0100

    update

--- birthday project/src/App.js
+++ birthday project/src/App.js
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 function App() {
-  const [people,setPeople] = useState(data)
-  return <main>
-  <section className='container'>
-    <h3>0 Birthday today</h3>
-    <List />
-    <button onClick={() => console.log('you clicked me !')}> clear all</button>
-  </section>
-  
-  </main>;
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
