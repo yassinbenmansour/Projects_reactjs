ad6fd66f6da78a8a5571795f8873dd354532dbd3
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-08-28 02:24:51 +0100

    update

--- birthday project/src/App.js
+++ birthday project/src/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
 import data from './data'
 import List from './List'
+
 function App() {
+
+
+  
   const [people, setPeople] = useState(data)
   return (
+
     <main>
       <section className='container'>
         <h3>{people.length} User in data</h3>
@@ -11,7 +16,11 @@ function App() {
         <button onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
-  )
+
+
+
+)
 }
+
 
 export default App
