0a1f0c795528bf3b8cd4c358449083bded5e0a3b
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-08-30 01:48:30 +0100

    update

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -18,18 +18,23 @@ class App extends React.Component{
       console.log("component did update");
     }
 
+    submitForm = (event) => {
+      event.preventDefault();
+      console.log(this.state);
+    };
+
   render(){
     console.log(this.state);
 
     return(
         <div> 
-          <form method='post'>
+          <form onSubmit={(event) => this.submitForm(event)} method='post'>
             <input type="text" name='name' id='' autoComplete='off' placeholder="Nom et prénom" 
             onChange={(event) => this.setState({ ...this.state,name : event.target.value})} />
 
             <input type="text" id='' autoComplete='off' placeholder="Age"
             onChange={(event) => this.setState({ ...this.state, age: event.target.value })} />
-            <button type=""> Validé</button>
+            <button type="submit"> Validé</button>
 
           </form>
         </div>
