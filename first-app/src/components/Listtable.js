cac23ab4cb0e19a925d12d03d5c31000acd0c306
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-12-28 02:04:36 +0100

    update

--- first-app/src/components/Listtable.js
+++ first-app/src/components/Listtable.js
@@ -1,25 +1,46 @@
-import React from 'react'
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function MyTable() {
-    const [items, setItems] = useState([
-      { id: 1, name: 'Item 1' },
-      { id: 2, name: 'Item 2' },
-      { id: 3, name: 'Item 3' },
+export default function Listtable() {
+
+    const [listtable, setListtable] = useState([
+        {product:"kawkaw",categorie:"momalahat",prix:"10DH"},
+        {product:"louz",categorie:"momalahat",prix:"20DH"},
+        {product:"garg3",categorie:"momalahat",prix:"30DH"},
+        {product:"akajo",categorie:"momalahat",prix:"40DH"},
+        {product:"fraise",categorie:"fruit",prix:"50DH"},
+        {product:"pomme",categorie:"fruit",prix:"70DH"}
     ]);
-  
-    const tableRows = items.map((item) => (
-      <tr key={item.id}>
-        <td>{item.name}</td>
-      </tr>
-    ));
-  
-    return (
-      <table className='table table-danger'>
-        <tbody>
-          {tableRows}
-        </tbody>
-      </table>
-    );
-  }
-  
+
+    
+
+  return (
+        
+    <>
+      <h1>    Table product :</h1>
+        <table className="table table-bordered ">
+           
+           <tr className="table-primary bg-danger">
+            <th>id</th>
+            <th>product</th>
+            <th>categorie</th>
+            <th>prix</th>
+           </tr>
+
+
+      {
+        listtable.map((element,id)=>{
+          return <tr key={id}>
+            <td>{id}</td>
+            <td>{element.product}</td>
+            <td>{element.categorie}</td>
+            <td>{element.prix}</td>
+          </tr>
+
+        })
+      }
+           
+        </table>
+    
+    </>
+  )
+}
