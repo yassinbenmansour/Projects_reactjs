38680013093019b2876729b28653a67639263535
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2023-01-02 17:43:36 +0100

    update

--- first-app/src/components/Useffectcount.js
+++ first-app/src/components/Useffectcount.js
@@ -2,23 +2,21 @@ import React, { useEffect, useState } from 'react'
 
 export default function Useffectcount() {
 
-    const [nbr,setNbr] = useState(0);
+  const [nbr,setNbr] = useState(0);
 
-    
 
-    useEffect(() => {
-        document.title = `You clicked ${nbr} times`;
-      }, [nbr]);
+  useEffect(()=> {
+    document.title = `You clicked ${nbr} times`;
+  }, [nbr])
+
 
 
   return (
     <div>
+        <h1>useEffect</h1>
+        <p>count = {nbr} </p>
+        <input type="button" onClick={()=>setNbr(nbr + 1)} value="click"/>
 
-                
-        <p>You clicked {nbr} times</p>
-            <button onClick={() => setNbr(nbr + 1)}>
-                Click me
-            </button>
 
     </div>
   )
